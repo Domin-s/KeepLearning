60db34a4b4bd97f3c5d1f2a015250d0bfc712e9d
Adrian Domin <adriandominx@gmail.com>
2023-10-03 15:17:39 +0200

    Add registration and login by adding package: Identity

--- KeepLearning.MVC/Program.cs
+++ KeepLearning.MVC/Program.cs
@@ -1,7 +1,13 @@
+using KeepLearning.Infrastructure.Extensions;
+using KeepLearning.Application.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
@@ -23,5 +29,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
