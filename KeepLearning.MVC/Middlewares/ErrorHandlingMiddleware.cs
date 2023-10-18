39f8369962c6877d72a45ff0795a73bbb653430f
Adrian Domin <adriandominx@gmail.com>
2023-10-18 11:50:18 +0200

    Add exception and create middleware

---
+++ KeepLearning.MVC/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using RestaurantAPI.Exceptions;
+
+namespace KeepLearning.MVC.Middlewares
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (BadRequestException badRequestException)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequestException.Message);
+            }
+            catch (ForbiddenExcepetion forbiddenException)
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync(forbiddenException.Message);
+            }
+            catch (NotFoundException notFoundException)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFoundException.Message);
+            }
+            catch (AlreadyExists alreadyExist)
+            {
+                context.Response.StatusCode = 409;
+                await context.Response.WriteAsync(alreadyExist.Message);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Soemthing went wrong");
+            }
+        }
+    }
+}
