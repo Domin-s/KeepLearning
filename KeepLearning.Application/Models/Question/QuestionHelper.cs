adea452162a1f130abaeb27373d3b74aca98bd62
Adrian Domin <adriandominx@gmail.com>
2023-10-25 12:42:23 +0200

    Change name for class

---
+++ KeepLearning.Application/Models/Question/QuestionHelper.cs
@@ -0,0 +1,35 @@
+﻿using KeepLearning.Application.Models.Enums;
+
+namespace KeepLearning.Application.Models.Question
+{
+    public static class QuestionHelper
+    {
+        public static QuestionDto FromCountryAndGuessType(Domain.Enteties.Country country, GuessType.Value guessType, int numberOfQuestion)
+        {
+            switch (guessType)
+            {
+                case GuessType.Value.CapitalCity:
+                    return new QuestionDto(numberOfQuestion, country.Name);
+
+                case GuessType.Value.Country:
+                    return new QuestionDto(numberOfQuestion, country.CapitalCity);
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public static IEnumerable<QuestionDto> FromCountriesAndGuessType(IEnumerable<Domain.Enteties.Country> countries, GuessType.Value guessType)
+        {
+            var questions = new List<QuestionDto>();
+            var numberQuestion = 0;
+
+            foreach (var item in countries)
+            {
+                questions.Add(FromCountryAndGuessType(item, guessType, numberQuestion++));
+            }
+
+            return questions;
+        }
+    }
+}
