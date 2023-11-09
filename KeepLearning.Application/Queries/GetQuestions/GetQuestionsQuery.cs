238f5c22d983613030e3e9ad3227ac0962e05450
Adrian Domin <adriandominx@gmail.com>
2023-11-09 14:40:03 +0100

    Remove extension from GetQuestionQuery

--- KeepLearning.Application/Queries/GetQuestions/GetQuestionsQuery.cs
+++ KeepLearning.Application/Queries/GetQuestions/GetQuestionsQuery.cs
@@ -1,9 +1,14 @@
-﻿using KeepLearning.Domain.Models.Test.Country;
+﻿using KeepLearning.Domain.Models.Enums;
+using KeepLearning.Domain.Models.Test.Country;
 using MediatR;
 
 namespace KeepLearning.Domain.Queries.GetQuestions
 {
-    public class GetQuestionsQuery : TestCountryDto, IRequest<TestCountryDto>
+    public class GetQuestionsQuery : IRequest<TestCountryDto>
     {
+        public string? Name { get; set; }
+        public int NumberOfQuestion { get; set; } = 10;
+        public GuessType.Value GuessType { get; set; } = default!;
+        public IEnumerable<Continent.Name> Continents { get; set; } = new List<Continent.Name>();
     }
 }
