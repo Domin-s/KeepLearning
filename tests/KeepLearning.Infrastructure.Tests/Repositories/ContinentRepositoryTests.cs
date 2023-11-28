ec0c2ac312392193750151bc053eff0b1d4c54fe
Adrian Domin <adriandominx@gmail.com>
2023-11-28 18:07:11 +0100

    Add test checking getting all continents

---
+++ tests/KeepLearning.Infrastructure.Tests/Repositories/ContinentRepositoryTests.cs
@@ -0,0 +1,41 @@
+using KeepLearning.Domain.Interfaces;
+using KeepLearning.Infrastructure.Persistence;
+using KeepLearning.Infrastructure.Repositories;
+using KeepLearning.Infrastructure.Tests.Helper.Seeders;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeepLearning.Infrastructure.Tests.Repositories
+{
+    public class ContinentRepositoryTests
+    {
+
+        private readonly KeepLearningDbContext _dbContext;
+        private readonly IContinentRepository _continentRepository;
+
+        public ContinentRepositoryTests()
+        {
+            var builder = new DbContextOptionsBuilder<KeepLearningDbContext>();
+            builder.UseInMemoryDatabase("TestKeepLearningDb-ContinetnRepository");
+
+            _dbContext = new KeepLearningDbContext(builder.Options);
+            var continentSeederTest = new ContinentSeederTest(_dbContext);
+            continentSeederTest.Seed();
+
+            _continentRepository = new ContinentRepository(_dbContext);
+        }
+
+        [Fact()]
+        public async void GetAll_ShouldReturnAllContinentsFromDatabase()
+        {
+            // arrange
+            var numberOfAllCountries = 6;
+
+            // act
+            var allCountries = await _continentRepository.GetAll();
+
+            // assert
+            allCountries.Should().NotBeNull();
+            allCountries.Count().Should().Be(numberOfAllCountries);
+        }
+    }
+}
