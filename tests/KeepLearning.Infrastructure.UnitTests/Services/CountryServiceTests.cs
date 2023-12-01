f2db412cafd5eeb2bafd79c59ec7871e53ae5d12
Adrian Domin <adriandominx@gmail.com>
2023-12-01 14:09:31 +0100

    Update tests

--- tests/KeepLearning.Infrastructure.UnitTests/Services/CountryServiceTests.cs
+++ tests/KeepLearning.Infrastructure.UnitTests/Services/CountryServiceTests.cs
@@ -1,7 +1,12 @@
 using KeepLearning.Domain.Enteties;
 using KeepLearning.Domain.Interfaces;
+using KeepLearning.Infrastructure.Repositories.UnitTests;
+using KeepLearning.Infrastructure.Repositories;
 using Moq;
 using static KeepLearning.Domain.Models.Enums.GuessType;
+using System.Diagnostics.Metrics;
+using static KeepLearning.Infrastructure.Repositories.UnitTests.CountryRepositoryTests;
+using KeepLearning.Domain.Models;
 
 namespace KeepLearning.Infrastructure.Services.UnitTests
 {
@@ -119,6 +124,76 @@ namespace KeepLearning.Infrastructure.Services.UnitTests
             result.Should().Be(false);
         }
 
+        [Fact()]
+        public async void GetRandomCountry_ForValidContinent_ReturnRadnomCountryWithGivenContinent()
+        {
+            // arrange
+            var continentIds = new List<Guid>() { Guid.NewGuid() };
+            var continentId = continentIds.First();
+
+            var countries = new List<Country>() {
+                new Country()
+                {
+                    Name = "Australia",
+                    Abbreviation = "AUS",
+                    CapitalCity = "Canberra",
+                    ContinentId = continentId
+                }
+            };
+
+            var countryRepositoryMock = new Mock<ICountryRepository>();
+            countryRepositoryMock.Setup(rep => rep.GetByContinents(continentIds)).ReturnsAsync(countries);
+
+            var countryService = new CountryService(countryRepositoryMock.Object);
+
+
+            // act
+            var randomCountry = await countryService.GetRandom(continentId);
+
+            // assert
+            randomCountry.Should().NotBeNull();
+            randomCountry!.ContinentId.Should().Be(continentId);
+        }
+
+        [Theory()]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void GetRandomCountries_ForValidContinent_ReturnRadnomCountriesWithoutDuplicate(int numberOfElemetns)
+        {
+            // arrange
+            var continentIds = new List<Guid>() { Guid.NewGuid() };
+            var continentId = continentIds.First();
+
+            var countries = new List<Country>() {
+                new Country()
+                {
+                    Name = "Croatia",
+                    Abbreviation = "HRV",
+                    CapitalCity = "Zagreb",
+                    ContinentId = continentId
+                },
+                new Country()
+                {
+                    Name = "Greece",
+                    Abbreviation = "GRC",
+                    CapitalCity = "Athens",
+                    ContinentId = continentId
+                },
+            };
+
+            var countryRepositoryMock = new Mock<ICountryRepository>();
+            countryRepositoryMock.Setup(rep => rep.GetByContinents(continentIds)).ReturnsAsync(countries);
+
+            var countryService = new CountryService(countryRepositoryMock.Object);
+
+            // act
+            var randomCountries = await countryService.RandomCountries(continentIds, numberOfElemetns);
+
+            // assert
+            randomCountries.Should().NotBeNull();
+            randomCountries.Count().Should().Be(numberOfElemetns);
+            randomCountries.Distinct().Count().Should().Be(numberOfElemetns);
+        }
 
         private string GetQuestionText(Country country, Category categry)
         {
