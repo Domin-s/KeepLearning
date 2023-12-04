d7f1c268868b8f81599a9ebfbbb164a2dc0ace33
Adrian Domin <adriandominx@gmail.com>
2023-12-04 17:04:41 +0100

    Add select to choose continent on list of countries

--- src/KeepLearning.MVC/Controllers/CountryController.cs
+++ src/KeepLearning.MVC/Controllers/CountryController.cs
@@ -1,5 +1,7 @@
-﻿using KeepLearning.Application.Country.Queries.GetAllCountriesByContinents;
+﻿using KeepLearning.Application.Continent.Queries.GetAllContinents;
+using KeepLearning.Application.Country.Queries.GetAllCountriesByContinents;
 using KeepLearning.Application.Country.Queries.GetNumberOfCountries;
+using KeepLearning.MVC.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,8 +20,11 @@ namespace KeepLearning.MVC.Controllers
         public async Task<IActionResult> List(GetAllCountriesByContinentsQuery query)
         {
             var countries = await _mediator.Send(query);
+            var continents = await _mediator.Send(new GetAllContinentsQuery());
 
-            return View(countries);
+            var viewModel = new ListOfCountriesModelView(countries, continents);
+
+            return View(viewModel);
         }
 
         [HttpPost]
