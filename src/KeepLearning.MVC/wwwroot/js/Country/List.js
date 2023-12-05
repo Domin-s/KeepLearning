c97b62045a5b2aa17d3dd7b3f993f48430f34247
Adrian Domin <adriandominx@gmail.com>
2023-12-05 10:49:34 +0100

    Add filter by continents in frontend

--- src/KeepLearning.MVC/wwwroot/js/Country/List.js
+++ src/KeepLearning.MVC/wwwroot/js/Country/List.js
@@ -1,3 +1,30 @@
 $(document).ready(function () {
+    var continentSelect = $('.continentSelect');
+    var continentRows = $('.continent');
 
+    continentSelect.click(function (event) {
+        HideOrShowContinents(continentSelect, continentRows);
+    });
+
+    function HideOrShowContinents(continents) {
+        for (let i = 0; i < continents.length; i++) {
+            var continentnName = GetFirstPartOfContientName(continents.get(i));
+            var rows = $('.' + continentnName);
+            var isChecked = continents.get(i).checked
+
+            HideOrShowRowsWithCountry(isChecked, rows);
+        }
+    }
+
+    function GetFirstPartOfContientName(continent) {
+        return continent.value.split(/(\s+)/)[0];
+    }
+
+    function HideOrShowRowsWithCountry(isChecked, rows) {
+        if (isChecked) {
+            rows.show();
+        } else {
+            rows.hide();
+        }
+    }
 });
