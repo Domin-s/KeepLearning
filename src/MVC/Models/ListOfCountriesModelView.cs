63ac6f29760759dddb1df6411fea149bb352d2ba
Adrian Domin <adriandominx@gmail.com>
2023-12-13 09:18:02 +0100

    Remove MVC module

--- src/MVC/Models/ListOfCountriesModelView.cs
+++
@@ -1,7 +0,0 @@
-﻿using Application.Common.Models.Continent;
-using Application.Common.Models.Country;
-
-namespace MVC.Models
-{
-    public record ListOfCountriesModelView(IEnumerable<CountryDto> CountryDtos, IEnumerable<ContinentDto> Continents) { }
-}
