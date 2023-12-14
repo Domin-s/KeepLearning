a599947620333ac4978e6af1a0c210c8429d890f
Adrian Domin <adriandominx@gmail.com>
2023-12-14 13:31:37 +0100

    Remove example things from main component

--- src/Web/Client/src/app/app.component.ts
+++ src/Web/Client/src/app/app.component.ts
@@ -14,23 +14,9 @@ interface WeatherForecast {
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
   }
 
   title = 'client';
