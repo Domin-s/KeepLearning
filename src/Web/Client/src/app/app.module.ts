003f0f799480280bc1ef41c021f0017d5213ec1b
Adrian Domin <adriandominx@gmail.com>
2023-12-14 12:50:44 +0100

    Change to cscc angular.json and add boostrap to main component

--- src/Web/Client/src/app/app.module.ts
+++ src/Web/Client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,8 +17,10 @@ import { TableOfCountriesComponent } from './country/components/table-of-countri
     TableOfCountriesComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule, 
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
