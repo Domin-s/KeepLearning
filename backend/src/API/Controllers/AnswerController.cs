28daf5bcc31ac6a43cd3f745832db929c266c5af
Adrian Domin <adriandominx@gmail.com>
2023-12-15 13:44:44 +0100

    Move all backend thind to backend folder

---
+++ backend/src/API/Controllers/AnswerController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Question.Queries.CheckAnswer;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnswerController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+        private readonly ILogger<AnswerController> _logger;
+
+        public AnswerController(IMediator mediator, ILogger<AnswerController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpPost("check")]
+        public async Task<IActionResult> CheckAnswer(CheckAnswerQuery checkAnswerQuery)
+        {
+            var result = await _mediator.Send(checkAnswerQuery);
+
+            return Ok(result);
+        }
+    }
+}
