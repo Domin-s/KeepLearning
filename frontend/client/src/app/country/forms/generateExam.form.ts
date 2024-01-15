47d5ef99f94b39d1eec46893fa429b25a7186f92
Adrian Domin <adriandominx@gmail.com>
2024-01-15 12:11:09 +0100

    Refactor form to generate exam

--- frontend/client/src/app/country/forms/generateExam.form.ts
+++ frontend/client/src/app/country/forms/generateExam.form.ts
@@ -1,14 +1,16 @@
 import { Injectable, inject } from "@angular/core"
-import { FormBuilder } from "@angular/forms"
+import { FormBuilder, Validators } from "@angular/forms"
 
 @Injectable()
 export class GenerateExamForm {
-    readonly form = inject(FormBuilder).group({
-        Name: [''],
-        NumberOfQuestion: [10],
-        Category: ['Country'],
-        Continents: ['Europe']
-    });
-
-    constructor(){}
+    form;
+   
+    constructor(private formBuilder: FormBuilder) {
+        this.form = this.formBuilder.group({
+            Name: [''],
+            NumberOfQuestion: [10, Validators.required],
+            Category: [0, Validators.required],
+            Continents: this.formBuilder.array([this.formBuilder.control('Europe')], Validators.required)
+        })
+    }
 }
