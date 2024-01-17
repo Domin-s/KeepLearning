5e111d4054bdb9cdb86d215baefa56d4bbf0f547
Adrian Domin <adriandominx@gmail.com>
2024-01-17 10:25:02 +0100

    Clean code and add forms elements to question-table

--- frontend/client/src/app/country/shared/question/question-table/question-table.component.ts
+++ frontend/client/src/app/country/shared/question/question-table/question-table.component.ts
@@ -1,15 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Question } from '../../../models/Question';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-question-table',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './question-table.component.html',
   styleUrl: './question-table.component.scss'
 })
-export class QuestionTableComponent{
+export class QuestionTableComponent {
   @Input({ required: true }) questions: Question[] = [];
   @Input({ required: true }) questionCategory: string = "";
   @Input({ required: true }) answerCategory: string = "";
+
+  isChecked = false;
+
+  checkExamForm: FormGroup;
+
+  constructor(){
+    this.checkExamForm = new FormGroup({
+      'Category': new FormControl(''),
+      'Answers': new FormArray([])
+    });
+  }
+
+  onSubmit(){
+    console.log(this.checkExamForm.value);
+    this.isChecked = true;
+  }
 }
